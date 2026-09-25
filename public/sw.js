9d00dd18afd74d11d14ee12016d3997001c24510
annbellfashions-sudo <annbellfashions@gmail.com>
2026-09-25 11:34:41 +0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'annbell-app-v3';
+const CACHE_NAME = 'annbell-app-v4-branding';
 const OFFLINE_URL = '/index.html';
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll([
-      '/', '/index.html', '/manifest.webmanifest', '/icon.svg'
+      '/', '/index.html', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png', '/logo.jpg'
     ])).then(() => self.skipWaiting())
   );
 });
